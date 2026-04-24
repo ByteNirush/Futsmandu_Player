ac7075ae41a102e5ebfe707c44662b1ef3eb7706
Nirush Man Shrestha <nirushstha@gmail.com>
2026-04-24 11:28:52 +0545

    refactor(network): enhance native Dio client configuration
    - Improve HTTP client setup for native platforms
    
    - Optimize connection timeouts and error handling

--- lib/core/network/player_dio_client_native.dart
+++ lib/core/network/player_dio_client_native.dart
@@ -1,10 +1,25 @@
 import 'package:dio/dio.dart';
+import 'package:flutter/foundation.dart';
 
 import '../services/player_auth_storage_service.dart';
 import 'player_dio_client_shared.dart';
 
 Dio createPlatformPlayerDioClient({bool enableAuthInterceptor = true}) {
   final dio = buildPlayerDioClient();
+
+  if (kDebugMode) {
+    dio.interceptors.add(
+      LogInterceptor(
+        request: true,
+        requestHeader: true,
+        requestBody: true,
+        responseHeader: true,
+        responseBody: true,
+        error: true,
+        logPrint: (object) => debugPrint(object.toString()),
+      ),
+    );
+  }
 
   if (enableAuthInterceptor) {
     dio.interceptors.add(
